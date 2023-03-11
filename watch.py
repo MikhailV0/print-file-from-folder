ad31fdf8d8a589028b7f821bce3125135499e088
Mikhail Volokhov <mikhail@volokhov.pro>
2023-03-11 17:13:34 +0300

    Fixed working with temporary files *.part

--- watch.py
+++ watch.py
@@ -10,19 +10,33 @@ import logging.config
 logging.config.fileConfig('loging.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
+
 class MonitorFolder(FileSystemEventHandler):
     def on_created(self, event):
-        # print(event.src_path, event.event_type)
-        if os.path.isfile(event.src_path):
-            #timeout for smb share folders //test mode//
+        filename, file_extension = os.path.splitext(event.src_path)
+        print(f'{filename}, {file_extension}')
+        if file_extension != '.part':
+            print('OK')
+            file_handler(event.src_path)
+
+    def on_moved(self, event):
+        print(event.dest_path)
+        #Linux when copy to smb create temp *.part then rename file
+        file_handler(event.dest_path)
+
+
+def file_handler(file_path: str) -> None:
+    if os.path.isfile(file_path):
+        # timeout for smb share folders //test mode//
+        time.sleep(5)
+        if print_file(file_path):
+            logger.info(f'Файл {file_path} наверное напечатан')
             time.sleep(15)
-            if print_file(event.src_path):
-                logger.info(f'Файл {event.src_path} наверное напечатан')
-                time.sleep(30)
-                if backup_file(event.src_path):
-                    logger.info(f'Файл {event.src_path} наверное перемещен')
-            else:
-                logger.warning(f'Что то пошло не так с файлом {event.src_path}')
+            if backup_file(file_path):
+                logger.info(f'Файл {file_path} наверное перемещен')
+        else:
+            logger.warning(f'Что то пошло не так с файлом {file_path}')
+
 
 def monitor_folder(path: str):
     """Monitoring new files in path, and print it"""
